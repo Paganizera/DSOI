e0d3813591e67a7da652f7c1c9e225433d39b653
Prog-in <joaogabrielferes@gmail.com>
2023-10-08 22:17:56 -0300

    add specific prompt for password

--- displays/users_display.py
+++ displays/users_display.py
@@ -1,5 +1,6 @@
 from .display_abstract import DisplayAbstract
 from controllers.controllers_abstract import ControllersAbstract
+from getpass import getpass
 
 
 class UsersDisplay(DisplayAbstract):
@@ -15,7 +16,7 @@ class UsersDisplay(DisplayAbstract):
             print('\t1 - Login')
             print('\t2 - Sign in')
             print('\t3 - Exit')
-            option = input('Option: ')
+            option = input('Option: ').strip()
             
             if not self.is_valid_input(option, range(1, 4)):
                 print('Invalid option!')
@@ -27,22 +28,23 @@ class UsersDisplay(DisplayAbstract):
             self.show_display_header('Users Menu')
             print('\t1 - Logout')
             print('\t2 - Update user info')
-            print('\t3 - Delete user')
-            print('\t4 - Go to main menu')
+            print('\t3 - Show user data')
+            print('\t4 - Delete user')
+            print('\t5 - Go to main menu')
             option = input('Option: ').strip()
 
-            if not self.is_valid_input(option, range(1, 5)):
+            if not self.is_valid_input(option, range(1, 6)):
                 print('Invalid option!')
             else:
                 return option
 
     def get_data(self) -> tuple[str, str]:
         nickname = input('Nickname: ').strip()
-        password = input('Password: ')
+        password = getpass('Password: ')
         return nickname, password
 
     def get_current_password(self) -> str:
-        return input('Current password: ')
+        return getpass('Current password: ')
 
     def show_user_data(self, nickname: str, password: str) -> None:
         print(f'Nickname: \"{nickname}\"')
@@ -50,3 +52,6 @@ class UsersDisplay(DisplayAbstract):
         
     def show_message(self, message: str) -> None:
         print(message)
+
+    def show_error(self, error: str) -> None:
+        print('ERROR: '+ error)
