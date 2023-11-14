e3d90dc1e4544b0f10b8a8b12eeee689f92da855
godogani <brunobianchipagani@gmail.com>
2023-11-13 21:04:37 -0300

    PATHFINDER

--- daos/user_dao.py
+++ daos/user_dao.py
@@ -1,11 +1,13 @@
 from .dao_abstract import DAO
 from uuid import UUID
 from entities.user import User
+from pathlib import Path
 
 #cada entidade terá uma classe dessa, implementação bem simples.
 class UserDAO(DAO):
     def __init__(self):
-        super().__init__('../source/users.pkl')
+        path = Path().parent / "source"/ "users.pkl"
+        super().__init__(str(path))
 
     def add(self, user: User):
         if((user is not None) and isinstance(user, User) and isinstance(user.id, UUID)): 
